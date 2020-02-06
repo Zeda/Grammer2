d7850a13c4016970231282b7521792bf682efe7b
NonstickAtom785 <36454623+NonstickAtom785@users.noreply.github.com>
2020-02-06 11:35:44 -0600

    Last fix
    Fixed the trying to compile if file non existent.

--- compile.bat
+++ compile.bat
@@ -15,45 +15,44 @@ rem Generates the jumptable and grammer2.5.inc file together.
   python ..\tools\jt.py jmptable.z80 grammer2.5.inc
   copy /Y grammer2.5.inc ..\docs\grammer2.5.inc
 
-:Grammer
+
+:app
   set /P c=Compile Grammer?(Y/N)
   if /I "%c%" EQU "Y" (
-
     echo "Assembling App"
     ..\spasm.exe grammer.z80 ..\bin\grammer.8xk -I ..\z80float\single
-    
+    if exist grampkg.z80 goto :grampkg
   )
 
   if /I "%c%" EQU "N" goto :grampkg
-goto :Grammer
+goto :app
 
 :grampkg
   set /P c=Compile Default Package?(Y/N)
   if /I "%c%" EQU "Y" (
     echo "Assembling Default Package"
     ..\spasm.exe grampkg.z80 ..\bin\grampkg.8xv
+    if exist experimental\experimental.z80 goto :exppkg
   )
 
   if /I "%c%" EQU "N" goto :exppkg
 goto :grampkg
 
 
-rem Check for an experimental package
-
-if exist experimental\experimental.z80 (
+rem Install experimental package
 :exppkg
   set /P c=Compile Experimental Package?(Y/N)
   if /I "%c%" EQU "Y" (
      	echo "Assembling Experimental Package"
    	..\spasm.exe experimental\experimental.z80 ..\bin\EXPRMNTL.8xv -I single
+    if exist launch.z80 goto :proglaunch
   )
 
   if /I "%c%" EQU "N" goto :proglaunch
 goto :exppkg
-)
 
-rem Check for the program launcher
-if exist launch.z80 (
+
+rem Install program launcher
 :proglaunch
   set /P c=Compile Program Launcher?(Y/N)
   if /I "%c%" EQU "Y" (
@@ -63,7 +62,6 @@ if exist launch.z80 (
   
   if /I "%c%" EQU "N" goto :exit
 goto :proglaunch
-)
 
 :exit
 cd..
