8117ddd0691f88ed7694f4eba72a609024e1f040
NonstickAtom785 <36454623+NonstickAtom785@users.noreply.github.com>
2020-02-06 10:39:46 -0600

    Add Program Launcher
    Added program launcher to the compilation process and options for whether or not to compile different things.

--- compile.bat
+++ compile.bat
@@ -9,20 +9,52 @@ if exist spasm.exe (
 
 cd src
 
+:Grammer
+set /P c=Compile Gramme?(Y/N)
+if /I "%c%" EQU "Y" (
 echo "Generating grammer2.5.inc"
 python ..\tools\jt.py jmptable.z80 grammer2.5.inc
-copy /Y grammer2.5.inc ..\docs\grammer2.5.inc
-
-echo "Assembling Default Package"
-..\spasm.exe grampkg.z80 ..\bin\grampkg.8xv
+copy grammer2.5.inc ..\docs\grammer2.5.inc
 
 echo "Assembling App"
 ..\spasm.exe grammer.z80 ..\bin\grammer.8xk -I ..\z80float\single
+)
+if /I "%c%" EQU "N" goto :grampkg
+goto :Grammer
+
+:grampkg
+set /P c=Compile Default Package?(Y/N)
+if /I "%c%" EQU "Y" (
+echo "Assembling Default Package"
+..\spasm.exe grampkg.z80 ..\bin\grampkg.8xv
+)
+if /I "%c%" EQU "N" goto :exppkg
+goto :grampkg
+
 
 rem Check for an experimental package
 if exist experimental\experimental.z80 (
-  echo "Assembling Experimental Package"
-  ..\spasm.exe experimental\experimental.z80 ..\bin\EXPRMNTL.8xv -I single
+:exppkg
+  set /P c=Compile Experimental Package?(Y/N)
+  if /I "%c%" EQU "Y" (
+     echo "Assembling Experimental Package"
+   ..\spasm.exe experimental\experimental.z80 ..\bin\EXPRMNTL.8xv -I single
+  )
+  if /I "%c%" EQU "N" goto :proglaunch
+    goto :exppkg
 )
 
+rem Check for the program launcher
+if exist launch.z80 (
+ :proglaunch
+  set /P c=Compile Program Launcher?(Y/N)
+  if /I "%c%" EQU "Y" (
+  echo "Assembling Program Launcher"
+  ..\spasm.exe launch.z80 ..\bin\g250.8xp
+ )
+ if /I "%c%" EQU "N" goto :exit
+ goto :proglaunch
+)
+
+:exit
 cd..
